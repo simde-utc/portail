b87cbbc2cf2afd81b4ca50a2f77761ff8805f7cb
Alex <abrasseur.pro@gmail.com>
2018-02-21 17:39:05 +0100

    Ajout du package de permission

--- config/app.php
+++ config/app.php
@@ -151,6 +151,7 @@ return [
          * Package Service Providers...
          */
         Barryvdh\Debugbar\ServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
