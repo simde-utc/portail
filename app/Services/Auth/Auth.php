6475b3382e179a12884f453393cca4cb29392347
NastuzziSamy <samy@nastuzzi.fr>
2018-02-28 19:46:05 +0100

    Fix de la structure de Auth et de Cas

--- app/Services/Auth/Auth.php
+++ app/Services/Auth/Auth.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Services\Auth;
-
 use Illuminate\Support\Facades\Auth as Authentification;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
@@ -9,93 +8,89 @@ use App\Models\UserPreferences;
 
 abstract class Auth
 {
-	protected $model = User::class;
+    protected static $namespace = 'App\\Models\\';
 
-	protected function findUser($key, $value, $use_specific_model = false) {
-		if ($use_specific_model) {
-			$model = self::$model;
-			return $model::where($key, $value)->first();
-		} else {
-			return User::where($key, $value)->first();
-		}
-	}
+    protected static function findUser($model, $key, $value) {
+        return (self::$namespace.$model)::where($key, $value)->first();
+    }
 
-	protected function create($email, $lastname, $firstname, $infos = []) {
-		// Création de l'utilisateur avec les informations minimales
-		$user = self::createUser($email, $lastname, $firstname);
+    protected static function create($model, $email, $lastname, $firstname, $infos = []) {
+        // Création de l'utilisateur avec les informations minimales
+        $user = self::createUser($email, $lastname, $firstname);
 
-		// On crée le système d'authentification
-		$auth = self::createAuth($user->id, $infos);
+        // On crée le système d'authentification
+        $userAuth = self::createAuth($model, $user->id, $infos);
 
-		// Si tout est bon, on le connecte
-		if ($user !== null && $auth !== null)
-			self::connect($user);
-	}
+        // Si tout est bon, on le connecte
+        if ($user !== null && $userAuth !== null)
+            self::connect($user);
+    }
 
-	protected function update($lastname, $firstname, $infos = []) {
-	  // Actualisation des informations
-	  $user = self::updateUser($lastname, $firstname);
+    protected static function update($model, $id, $lastname, $firstname, $infos = []) {
+        // Actualisation des informations
+        $user = self::updateUser($id, $lastname, $firstname);
 
-	  // On actualise le système d'authentification
-	  $auth = self::updateAuth($infos);
+        // On actualise le système d'authentification
+        $userAuth = self::updateAuth($model, $id, $infos);
 
-	  // Si tout est bon, on le connecte
-	  if ($user !== null && $auth !== null)
-		$this->connect();
-	}
+        // Si tout est bon, on le connecte
+        if ($user !== null && $userAuth !== null)
+            self::connect($user);
+    }
 
-	protected function createUser($email, $lastname, $firstname) {
-	  $user = User::create([
-		'email' => $email,
-		'lastname' => $lastname,
-		'firstname' => $firstname,
-		'last_login_at' => new \DateTime()
-	  ]);
+    protected static function createUser($email, $lastname, $firstname) {
+        $user = User::create([
+          'email' => $email,
+          'lastname' => $lastname,
+          'firstname' => $firstname,
+          'last_login_at' => new \DateTime()
+        ]);
 
-	  // Ajout dans les préférences
-	  $userPreferences = UserPreferences::create([
-		'user_id' => $this->getUserId(),
-		'email' => $email,
-	  ]);
+        // Ajout dans les préférences
+        $userPreferences = UserPreferences::create([
+          'user_id' => $user->id,
+          'email' => $email,
+        ]);
 
-	  return $user;
-	}
+        return $user;
+    }
 
-	protected static function updateUser($id, $lastname, $firstname) {
-	  $user = User::find($id);
-	  $user->lastname = $lastname;
-	  $user->firstname = $firstname;
-	  $user->save();
+    protected static function updateUser($id, $lastname, $firstname) {
+        $user = User::find($id);
+        $user->lastname = $lastname;
+        $user->firstname = $firstname;
+        $user->save();
 
-	  $user->timestamps = false;
-	  $user->last_login_at = new \DateTime();
-	  $user->save();
+        $user->timestamps = false;
+        $user->last_login_at = new \DateTime();
+        $user->save();
 
-	  return $user;
-	}
+        return $user;
+    }
 
-	protected static function createAuth($id, $infos = []) {
-	  $model = self::model;
+    protected static function createAuth($model, $id, $infos = []) {
+        return (self::$namespace.$model)::create(array_merge($infos, [
+          'user_id' => $id,
+          'last_login_at' => new \DateTime(),
+        ]));
+    }
 
-	  return $model::create(array_merge($infos, [
-		'user_id' => $id,
-		'last_login_at' => new \DateTime(),
-	  ]));
-	}
+    protected static function updateAuth($model, $id, $infos = []) {
+        $userAuth = (self::$namespace.$model)::find($id);
 
-	protected static function updateAuth($id, $infos = []) {
-	  $auth = (static::model)::find($id);
-	  foreach ($infos as $key => $value)
-		$auth->$key = $value;
+        foreach ($infos as $key => $value)
+          $userAuth->$key = $value;
 
-	  $auth->save();
+        $userAuth->save();
 
-	  $auth->timestamps = false;
-	  $auth->last_login_at = new \DateTime();
-	  $auth->save();
-	}
+        $userAuth->timestamps = false;
+        $userAuth->last_login_at = new \DateTime();
+        $userAuth->save();
 
-	protected static function connect($user) {
-	  Authentification::login($user);
-	}
+        return $userAuth;
+    }
+
+    protected static function connect($user) {
+        Authentification::login($user);
+    }
 }
--- app/Services/Auth/Cas.php
+++ app/Services/Auth/Cas.php
@@ -1,14 +1,12 @@
 <?php
 
 namespace App\Services\Auth;
-
-use App\Services\Auth\Auth;
 use App\Models\AuthCas;
 
 class Cas extends Auth
 {
 	protected const URL = 'https://cas.utc.fr/cas/';
-	protected $model = AuthCas::class;
+	protected static $model = 'AuthCas';
 
 	public static function authenticate($service, $ticket) {
 		if (!isset($ticket) || empty($ticket))
@@ -26,17 +24,18 @@ class Cas extends Auth
 		$userArray = $parsed->array['cas:serviceResponse']['cas:authenticationSuccess'];
 
 		// On cherche si l'utilisateur existe déjà dans la BDD
-		$auth = self::findUser('login', $userArray['cas:user'], true);
-		if ($auth === null) {
+		$userAuth = self::findUser(self::$model, 'login', $userArray['cas:user']);
+
+		if ($userAuth === null) {
 			// Si inconnu, on le crée et on le connecte.
-			self::create($userArray['cas:attributes']['cas:mail'], $userArray['cas:attributes']['cas:givenName'], $userArray['cas:attributes']['cas:sn'], [
+			self::create(self::$model, $userArray['cas:attributes']['cas:mail'], $userArray['cas:attributes']['cas:givenName'], $userArray['cas:attributes']['cas:sn'], [
 				'login' => $userArray['cas:user'],
 				'email' => $userArray['cas:attributes']['cas:mail'],
 			]);
 		}
 		else {
 			// Si connu, on actualise ses infos et on le connecte.
-			self::update($auth->user_id, $userArray['cas:attributes']['cas:givenName'], $userArray['cas:attributes']['cas:sn'], [
+			self::update(self::$model, $userAuth->user_id, $userArray['cas:attributes']['cas:givenName'], $userArray['cas:attributes']['cas:sn'], [
 				'login' => $userArray['cas:user'],
 				'email' => $userArray['cas:attributes']['cas:mail'],
 			]);
@@ -45,96 +44,86 @@ class Cas extends Auth
 		return true;
 	}
 
-	protected static function findUserFromLogin($login) {
-		return User::utc()->whereHas('auth_cas', function($query) {
-			$query->where('login', $login);
-		});
-	}
-
 	public static function login($service) {
 		return redirect()->away(self::URL.'login?service='.$service);
-	}
-
-	public static function logout() {
-		return redirect()->away(self::URL.'logout');
 	}
 
 	/* Pas besoin de logout() spécifique, le notre est géré par laravel */
 }
 
-class xmlToArrayParser
-{
-	/** The array created by the parser can be assigned to any variable: $anyVarArr = $domObj->array.*/
-	public  $array = array();
-	public  $parse_error = false;
-	private $parser;
-	private $pointer;
+// A l'air d'être plus stable que l'ancien
+class xmlToArrayParser {
+  /** The array created by the parser can be assigned to any variable: $anyVarArr = $domObj->array.*/
+  public  $array = array();
+  public  $parse_error = false;
+  private $parser;
+  private $pointer;
 
-	/** Constructor: $domObj = new xmlToArrayParser($xml); */
-	public function __construct($xml) {
-		$this->pointer =& $this->array;
-		$this->parser = xml_parser_create("UTF-8");
-		xml_set_object($this->parser, $this);
-		xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, false);
-		xml_set_element_handler($this->parser, "tag_open", "tag_close");
-		xml_set_character_data_handler($this->parser, "cdata");
-		$this->parse_error = xml_parse($this->parser, ltrim($xml))? false : true;
-	}
+  /** Constructor: $domObj = new xmlToArrayParser($xml); */
+  public function __construct($xml) {
+    $this->pointer =& $this->array;
+    $this->parser = xml_parser_create("UTF-8");
+    xml_set_object($this->parser, $this);
+    xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, false);
+    xml_set_element_handler($this->parser, "tag_open", "tag_close");
+    xml_set_character_data_handler($this->parser, "cdata");
+    $this->parse_error = xml_parse($this->parser, ltrim($xml))? false : true;
+  }
 
-	/** Free the parser. */
-	public function __destruct() { xml_parser_free($this->parser);}
+  /** Free the parser. */
+  public function __destruct() { xml_parser_free($this->parser);}
 
-	/** Get the xml error if an an error in the xml file occured during parsing. */
-	public function get_xml_error() {
-		if($this->parse_error) {
-			$errCode = xml_get_error_code ($this->parser);
-			$thisError =  "Error Code [". $errCode ."] \"<strong style='color:red;'>" . xml_error_string($errCode)."</strong>\",
-			at char ".xml_get_current_column_number($this->parser) . "
-			on line ".xml_get_current_line_number($this->parser)."";
-		}else $thisError = $this->parse_error;
-		return $thisError;
-	}
+  /** Get the xml error if an an error in the xml file occured during parsing. */
+  public function get_xml_error() {
+    if($this->parse_error) {
+      $errCode = xml_get_error_code ($this->parser);
+      $thisError =  "Error Code [". $errCode ."] \"<strong style='color:red;'>" . xml_error_string($errCode)."</strong>\",
+                            at char ".xml_get_current_column_number($this->parser) . "
+                            on line ".xml_get_current_line_number($this->parser)."";
+    }else $thisError = $this->parse_error;
+    return $thisError;
+  }
 
-	private function tag_open($parser, $tag, $attributes) {
-		$this->convert_to_array($tag, 'attrib');
-		$idx=$this->convert_to_array($tag, 'cdata');
-		if(isset($idx)) {
-			$this->pointer[$tag][$idx] = Array('@idx' => $idx,'@parent' => &$this->pointer);
-			$this->pointer =& $this->pointer[$tag][$idx];
-		}else {
-			$this->pointer[$tag] = Array('@parent' => &$this->pointer);
-			$this->pointer =& $this->pointer[$tag];
-		}
-		if (!empty($attributes)) { $this->pointer['attrib'] = $attributes; }
-	}
+  private function tag_open($parser, $tag, $attributes) {
+    $this->convert_to_array($tag, 'attrib');
+    $idx=$this->convert_to_array($tag, 'cdata');
+    if(isset($idx)) {
+      $this->pointer[$tag][$idx] = Array('@idx' => $idx,'@parent' => &$this->pointer);
+      $this->pointer =& $this->pointer[$tag][$idx];
+    }else {
+      $this->pointer[$tag] = Array('@parent' => &$this->pointer);
+      $this->pointer =& $this->pointer[$tag];
+    }
+    if (!empty($attributes)) { $this->pointer['attrib'] = $attributes; }
+  }
 
-	/** Adds the current elements content to the current pointer[cdata] array. */
-	private function cdata($parser, $cdata) { $this->pointer['cdata'] = trim($cdata); }
+  /** Adds the current elements content to the current pointer[cdata] array. */
+  private function cdata($parser, $cdata) { $this->pointer['cdata'] = trim($cdata); }
 
-	private function tag_close($parser, $tag) {
-		$current = & $this->pointer;
-		if(isset($this->pointer['@idx'])) {unset($current['@idx']);}
+  private function tag_close($parser, $tag) {
+    $current = & $this->pointer;
+    if(isset($this->pointer['@idx'])) {unset($current['@idx']);}
 
-		$this->pointer = & $this->pointer['@parent'];
-		unset($current['@parent']);
+    $this->pointer = & $this->pointer['@parent'];
+    unset($current['@parent']);
 
-		if(isset($current['cdata']) && count($current) == 1) { $current = $current['cdata'];}
-		else if(empty($current['cdata'])) {unset($current['cdata']);}
-	}
+    if(isset($current['cdata']) && count($current) == 1) { $current = $current['cdata'];}
+    else if(empty($current['cdata'])) {unset($current['cdata']);}
+  }
 
-	/** Converts a single element item into array(element[0]) if a second element of the same name is encountered. */
-	private function convert_to_array($tag, $item) {
-		if(isset($this->pointer[$tag][$item])) {
-			$content = $this->pointer[$tag];
-			$this->pointer[$tag] = array((0) => $content);
-			$idx = 1;
-		}else if (isset($this->pointer[$tag])) {
-			$idx = count($this->pointer[$tag]);
-			if(!isset($this->pointer[$tag][0])) {
-				foreach ($this->pointer[$tag] as $key => $value) {
-					unset($this->pointer[$tag][$key]);
-					$this->pointer[$tag][0][$key] = $value;
-				}}}else $idx = null;
-				return $idx;
-			}
+  /** Converts a single element item into array(element[0]) if a second element of the same name is encountered. */
+  private function convert_to_array($tag, $item) {
+    if(isset($this->pointer[$tag][$item])) {
+      $content = $this->pointer[$tag];
+      $this->pointer[$tag] = array((0) => $content);
+      $idx = 1;
+    }else if (isset($this->pointer[$tag])) {
+      $idx = count($this->pointer[$tag]);
+      if(!isset($this->pointer[$tag][0])) {
+        foreach ($this->pointer[$tag] as $key => $value) {
+            unset($this->pointer[$tag][$key]);
+            $this->pointer[$tag][0][$key] = $value;
+    }}}else $idx = null;
+    return $idx;
+  }
 }
