7591bbd94012fffe711a3bb1fc8b4017d4c2a788
tmeurou <thomas.meurou@yahoo.fr>
2018-03-12 22:27:08 +0100

    Ajout de la table article

---
+++ database/migrations/2018_03_12_211542_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+			$table->string('title', 128);
+			$table->longText('content');
+			$table->string('image', 128);
+			$table->boolean('toBePublished');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
