1019cb721125c08be06217dc43733fc92c821324
fejore <josselin.pennors@hotmail.fr>
2018-03-12 18:54:41 +0100

    Ajout de la table Room

---
+++ database/migrations/2018_03_12_174437_create_rooms_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->string('numero');
+            $table->string('name');
+            $table->string('pole');
+            
+            /* LIGNE A RAJOUTER QUAND TABLE ASSO CREE
+
+            ->references('login')->on('asso');
+            
+            */
+
+            $table->primary('numero');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
