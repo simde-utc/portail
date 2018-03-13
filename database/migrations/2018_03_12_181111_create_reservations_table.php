772b0478f53ffb64f5b946c824177693040f4646
fejore <josselin.pennors@hotmail.fr>
2018-03-13 11:04:31 +0100

    Ajout table Réservation

---
+++ database/migrations/2018_03_12_181111_create_reservations_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->string('salle')->references('numero')->on('rooms');
+            $table->string('asso');
+
+            /* A rajouter après création table Asso
+
+            ->references('numero')->on('Asso')
+
+            */
+
+            $table->timestamp('date_from');
+            $table->timestamp('date_to');
+            $table->string('id_user')->references('email')->on('users');
+            $table->string('comment', 250)->nullable();
+            $table->enum('type', ['reunion', 'logistique', 'autre']);
+            $table->primary(['salle', 'asso']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
