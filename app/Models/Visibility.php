94413720e7b42a47665e1f7df5bd9019aa044127
huetremy <remyhuet@gmail.com>
2018-03-13 18:24:11 +0100

    Ajout modèle Visibility

---
+++ app/Models/Visibility.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Visibility extends Model
+{
+    protected $table = 'visibilities';
+    
+    protected $fillable = [
+        'name',
+        ];
+
+    public function articles() {
+        return $this->hasMany('App\Models\Article');
+    }
+
+    public function events() {
+        return $this->hasMany('App\Models\Event');
+    }
+}
