b0ef5725e0a9028559807b3190ab31cd0e357768
huetremy <remyhuet@gmail.com>
2018-03-13 22:52:10 +0100

    Fin Article Model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
+
+    protected $table = 'articles';
+
+    protected $fillable = [
+        'title', 'content', 'image', 'toBePublished', 'visibility_id', 'asso_id'
+    ];
+
     public function assos() {
   		return $this->belongsToMany('App\Models\Asso', 'assos_articles');
   	}
