8b4ecd9d8df4c71c4058a86d8754011f9ef82d58
Natan Danous <natan.danous@gmail.com>
2018-03-14 10:37:43 +0100

    Création du controller et de ses méthodes
    Manque delete et la Request

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Group;
+//use App\Http\Requests\GroupRequest;
+
+class GroupController extends Controller
+{
+    public function __construct() {
+        // $this->middleware('auth:api');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $groups = Group::where('is_public', 1)->where('is_active', 1)->get();
+        return response()->json($groups, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $group = Group::create($request->input());
+        if ($group)
+            return response()->json($group, 200);
+        else
+            return response()->json(["message" => "Impossible de créer le groupe"], 500);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $group = Group::find($id);
+        if ($group)
+            return response()->json($group, 200);
+        else
+            return response()->json(["message" => "Impossible de trouver le groupe"], 404);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $group = Group::find($id);
+        if ($group)
+            $group = Group::update($request->input());
+            return response()->json($group, 200);
+        else
+            return response()->json(["message" => "Impossible de trouver le groupe"], 500);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // TODO : soft delete.
+    }
+}
