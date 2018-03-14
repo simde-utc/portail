a394cbe4015b2999968cd4ec6128207f7e4c0c01
huetremy <remyhuet@gmail.com>
2018-03-14 13:44:27 +0100

    Ajout de la request article

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true; //TODO : vérifier que l'utilisateur a les droits
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+			'title' => 'required|string|'.validation_between('title'),
+	        'content' => 'required|string|'.validation_between('article'),
+	        'image' => 'required|image|'.validation_between('url'),
+	        'toBePublished' => 'boolean',
+	        'visibility_id' => 'required|integer|exists:visibilities,id',
+        ];
+    }
+}
