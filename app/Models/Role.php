5934a0c37ab1a872507be67e926a899825a351d1
NastuzziSamy <samy@nastuzzi.fr>
2018-04-27 21:32:57 +0200

    Ajout de rôles spécifiques à un id précis
    @César ;)

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -11,18 +11,23 @@ class Role extends Model
 {
 	use HasPermissions;
 
-    public static function create(array $attributes = []) {
-        if (static::where('type', $attributes['type'])->first())
-			throw new \Exception('Ce rôle existe déjà');
+  public static function create(array $attributes = []) {
+      if (static::where('type', $attributes['type'])->first())
+		throw new \Exception('Ce rôle existe déjà');
 
-        return static::query()->create($attributes);
-    }
+      return static::query()->create($attributes);
+  }
 
 	public static function find(int $id, string $only_for = null) {
 		$roles = static::where('id', $id);
 
-		if ($only_for !== null)
-			$roles->where('only_for', $only_for);
+		if ($only_for !== null) {
+			$group = explode('-', $only_for)[0] ?? $only_for;
+
+			$roles->where(function ($query) use ($group, $only_for) {
+				$query->where('only_for', $group)->orWhere('only_for', $only_for);
+			});
+		}
 
 		return $roles->first();
 	}
@@ -30,24 +35,36 @@ class Role extends Model
 	public static function findByType(string $type, string $only_for = null) {
 		$roles = static::where('type', $type);
 
-		if ($only_for !== null)
-			$roles->where('only_for', $only_for);
+		if ($only_for !== null) {
+			$group = explode('-', $only_for)[0] ?? $only_for;
+
+			$roles->where(function ($query) use ($group, $only_for) {
+				$query->where('only_for', $group)->orWhere('only_for', $only_for);
+			});
+		}
 
 		return $roles->first();
 	}
 
 	public static function getRole($role, string $only_for = null) {
-        if (is_string($role))
-            return static::findByType($role, $only_for);
-        else if (is_int($role))
+    	if (is_string($role))
+    		return static::findByType($role, $only_for);
+    	else if (is_int($role))
 			return static::find($role, $only_for);
 		else
 			return $role;
 	}
 
 	public static function getRoles($roles, string $only_for = 'users') {
-		if (is_array($roles))
-			return static::whereIn('id', $roles)->orWhereIn('type', $roles)->where('only_for', $only_for)->get();
+		$group = explode('-', $only_for)[0] ?? $only_for;
+
+		if (is_array($roles)) {
+			return static::where(function ($query) use ($roles) {
+				$query->whereIn('id', $roles)->orWhereIn('type', $roles);
+			})->where(function ($query) use ($group, $only_for) {
+				$query->where('only_for', $group)->orWhere('only_for', $only_for);
+			})->get();
+		}
 		else if ($roles instanceof \Illuminate\Database\Eloquent\Model)
 			return collect($roles);
 		else
@@ -56,7 +73,6 @@ class Role extends Model
 
 	public static function getRoleAndItsParents($role, string $only_for = null) {
 		$role = static::getRole($role, $only_for);
-
 
 		if ($role === null)
 			return null;
@@ -67,30 +83,30 @@ class Role extends Model
 		return $roles;
 	}
 
-    public function permissions(): BelongsToMany {
-        return $this->belongsToMany(Permission::class, 'roles_permissions');
-    }
+  public function permissions(): BelongsToMany {
+      return $this->belongsToMany(Permission::class, 'roles_permissions');
+  }
 
-    public function users(): BelongsToMany {
-		return $this->belongsToMany(User::class, 'users_roles');
-    }
+  public function users(): BelongsToMany {
+	return $this->belongsToMany(User::class, 'users_roles');
+  }
 
-    public function childs(): BelongsToMany {
-		return $this->belongsToMany(Role::class, 'roles_parents', 'parent_id', 'role_id');
-    }
+  public function childs(): BelongsToMany {
+	return $this->belongsToMany(Role::class, 'roles_parents', 'parent_id', 'role_id');
+  }
 
-    public function parents(): BelongsToMany {
-		return $this->belongsToMany(Role::class, 'roles_parents', 'role_id', 'parent_id');
-    }
+  public function parents(): BelongsToMany {
+	return $this->belongsToMany(Role::class, 'roles_parents', 'role_id', 'parent_id');
+  }
 
-    public function hasPermissionTo($permission): bool {
-        if (is_string($permission))
-            $permission = Permission::findByType($permission);
-        else if (is_int($permission))
-            $permission = Permission::find($permission);
+  public function hasPermissionTo($permission): bool {
+      if (is_string($permission))
+          $permission = Permission::findByType($permission);
+      else if (is_int($permission))
+          $permission = Permission::find($permission);
 
-        return $this->permissions->contains('id', $permission->id);
-    }
+      return $this->permissions->contains('id', $permission->id);
+  }
 
 	public function givePermissionTo($permissions) {
 		$this->permissions()->withTimestamps()->attach(Permission::getPermissions(stringToArray($permissions), $this->only_for === 'users'));
