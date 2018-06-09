92d82ff4f6c852fec3f263dca6f0bd5526f38354
NastuzziSamy <samy@nastuzzi.fr>
2018-06-09 13:43:05 +0200

    Ajout des migrations des évènements

---
+++ database/migrations/2018_06_07_132423_create_calendars_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCalendarsEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendars_events', function (Blueprint $table) {
+            $table->increments('id');
+			$table->integer('calendar_id')->unsigned();
+			$table->foreign('calendar_id')->references('id')->on('calendars');
+			$table->integer('event_id')->unsigned();
+			$table->foreign('event_id')->references('id')->on('events');
+
+  			$table->timestamps();
+            $table->unique(['calendar_id', 'event_id']);
+  		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendars_events');
+    }
+}
