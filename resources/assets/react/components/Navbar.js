b9016e0b5e709c468a16cf4fabcf848e9a4794d4
Natan Danous <natan.danous@gmail.com>
2018-07-11 11:05:18 +0200

    Navbar : small design fixes

--- resources/assets/react/components/Navbar.js
+++ resources/assets/react/components/Navbar.js
@@ -6,58 +6,56 @@ class Navbar extends Component {
 	render() {
 		return (
 			<nav className="navbar navbar-expand-md navbar-dark fixed-top">
-				<div className="container">
-					<Link className="navbar-brand" to="/">Portail des Assos</Link>
-					<button className="navbar-toggler">
-						<span className="navbar-toggler-icon"></span>
-					</button>
+				<Link className="navbar-brand" to="/">Portail des Assos</Link>
+				<button className="navbar-toggler">
+					<span className="navbar-toggler-icon"></span>
+				</button>
 
-					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul className="navbar-nav">
-							<li className="nav-item">
-								<Link className="nav-link" to="/dashboard">Dashboard</Link>
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<Link className="nav-link" to="/dashboard">Dashboard</Link>
+						</li>
+					</ul>
+
+					<ul className="navbar-nav ml-auto">
+						{ this.props.isAuthenticated ? (
+							<li className="nav-item dropdown">
+								<a className="nav-link dropdown-toggle">
+									{ this.props.name } <span className="caret"></span>
+								</a>
+								<div className="dropdown-menu">
+									<a className="dropdown-item" href="/logout" onClick="event.preventDefault(); document.getElementById('logout-form').submit();">
+										Se déconnecter
+									</a>
+
+									<form id="logout-form" action="/logout" method="POST" style="display: none;">
+										csrf
+									</form>
+								</div>
 							</li>
-						</ul>
+						) : (
+							<li className="nav-item dropdown">
+								<a className="nav-link dropdown-toggle">
+									Se connecter <span className="caret"></span>
+								</a>
+								<div className="dropdown-menu">
+									<a className="dropdown-item" href="/login">Tout voir</a>
 
-						<ul className="navbar-nav ml-auto">
-							{ this.props.isAuthenticated ? (
-								<li className="nav-item dropdown">
-									<a className="nav-link dropdown-toggle">
-										{ this.props.name } <span className="caret"></span>
-									</a>
-									<div className="dropdown-menu">
-										<a className="dropdown-item" href="/logout" onClick="event.preventDefault(); document.getElementById('logout-form').submit();">
-											Se déconnecter
+								{/*
+									@foreach (config('auth.services') as $name => $provider)
+										<a className="dropdown-item" href="{{ route('login.show', ['provider' => $name, 'redirect' => $redirect ?? url()->previous()]) }}">
+											{{ $provider['name'] }}
 										</a>
-
-										<form id="logout-form" action="/logout" method="POST" style="display: none;">
-											csrf
-										</form>
-									</div>
-								</li>
-							) : (
-								<li className="nav-item dropdown">
-									<a className="nav-link dropdown-toggle">
-										Se connecter <span className="caret"></span>
-									</a>
-									<div className="dropdown-menu">
-										<a className="dropdown-item" href="/login">Tout voir</a>
-
-									{/*
-										@foreach (config('auth.services') as $name => $provider)
-											<a className="dropdown-item" href="{{ route('login.show', ['provider' => $name, 'redirect' => $redirect ?? url()->previous()]) }}">
-												{{ $provider['name'] }}
-											</a>
-										@endforeach
-											<a className="dropdown-item" href="{{ route('login', ['see' => 'all', 'redirect' => $redirect ?? url()->previous()]) }}">
-												Tout voir
-											</a>
-									*/}
-									</div>
-								</li>
-							)}
-						</ul>
-					</div>
+									@endforeach
+										<a className="dropdown-item" href="{{ route('login', ['see' => 'all', 'redirect' => $redirect ?? url()->previous()]) }}">
+											Tout voir
+										</a>
+								*/}
+								</div>
+							</li>
+						)}
+					</ul>
 				</div>
 			</nav>
 
