4c14f8f8a37e84aa45a82853890f52c8f7281c7d
NastuzziSamy <samy@nastuzzi.fr>
2018-07-15 20:01:19 +0200

    Amélioration de l'installation rapide

--- app/Console/Commands/Install.php
+++ app/Console/Commands/Install.php
@@ -18,7 +18,7 @@ class Install extends Command
 	 *
 	 * @var string
 	 */
-	protected $description = 'Install/update the whole application';
+	protected $description = 'Install the whole application';
 
 	/**
 	 * Create a new command instance.
@@ -30,6 +30,13 @@ class Install extends Command
 		parent::__construct();
 	}
 
+	protected function changeEnv(string $param, string $value) {
+		$param = str_replace('.', '\\.', str_replace('/', '\\/', $param));
+		$value = str_replace('.', '\\.', str_replace('/', '\\/', $value));
+
+		shell_exec('sed -i "s/'.$param.'=.*$/'.$param.'='.$value.'/g" .env');
+	}
+
 	/**
 	 * Execute the console command.
 	 *
@@ -37,7 +44,79 @@ class Install extends Command
 	 */
 	public function handle()
 	{
-		$bar = $this->output->createProgressBar(8);
+		$bar = $this->output->createProgressBar(9);
+		$bar->advance();
+
+		$this->info(' [Quick Install] Preparation');
+		$this->info(' [Quick Install] Preparation - Portail');
+		$subBar = $this->output->createProgressBar(14);
+		$subBar->advance();
+		$editEnv = true;
+
+		if (file_exists('.env')) {
+			$this->info(' /!\ An .env file already exists /!\\ ');
+
+			if ($this->confirm('Edit over it ?'))
+				shell_exec('cp .env .env.last');
+			else
+				$editEnv = false;
+		}
+
+		if ($editEnv) {
+			$value = $this->ask('App name ?', 'Portail des Associations');
+			$this->changeEnv('APP_NAME', '"'.$value.'"');
+			$subBar->advance();
+
+			$value = $this->choice('Environment ?', ['develop', 'production', 'testing'], 0);
+			$this->changeEnv('APP_ENV', $value);
+			$subBar->advance();
+
+			$value = $this->choice('Debug mode ?', ['true', 'false'], 0);
+			$this->changeEnv('APP_DEBUG', $value);
+			$subBar->advance();
+
+			$value = $this->ask('App url ?', 'http://localhost');
+			$this->changeEnv('APP_URL', $value);
+			$subBar->advance();
+
+			$value = $this->ask('App asso ?', 'simde');
+			$this->changeEnv('APP_ASSO', $value);
+			$subBar->advance();
+
+			$value = $this->ask('Ginger key ?', '');
+			$this->changeEnv('GINGER_KEY', $value);
+			$subBar->advance();
+
+			$this->info(' [Quick Install] Preparation - Database');
+			$subBar->advance();
+
+			$value = $this->choice('Type ?', ['mysql', 'pgsql', 'sqlite'], 0);
+			$this->changeEnv('DB_CONNECTION', $value);
+			$subBar->advance();
+
+			$value = $this->ask('Host ?', '127.0.0.1');
+			$this->changeEnv('DB_HOST', $value);
+			$subBar->advance();
+
+			$value = $this->choice('Port ?', ['3306', '5432'], 0);
+			$this->changeEnv('DB_PORT', $value);
+			$subBar->advance();
+
+			$value = $this->ask('Database ?', 'portail');
+			$this->changeEnv('DB_DATABASE', $value);
+			$subBar->advance();
+
+			$value = $this->ask('Username ?', 'portail');
+			$this->changeEnv('DB_USERNAME', $value);
+			$subBar->advance();
+
+			$value = $this->ask('Password ?', 'portail');
+			$this->changeEnv('DB_PASSWORD', $value);
+			$subBar->advance();
+		}
+		else
+			$subBar->finish();
+
 		$bar->advance();
 
 		// Installation
@@ -55,12 +134,9 @@ class Install extends Command
 		$bar->advance();
 
 		// Configuration
-		$this->info(' [Quick Install] Setting .env');
+		$this->info(' [Quick Install] Setting keys');
 		$this->call('key:generate');
 		$this->call('passport:keys');
-
-		// Copy .env and fill it up ?
-		// $env = $this->choice('What is the environment ?', ['development', 'testing', 'production'], 0);
 		$bar->advance();
 
 		// Migrating
@@ -69,6 +145,7 @@ class Install extends Command
 			$this->call('migrate:fresh');
 		else
 			$this->call('migrate');
+
 		if ($this->confirm('Seed the database ?'))
 			$this->call("db:seed");
 		$bar->advance();
