06435ccb0d154844793ce75dcb35fd2d536ba2dd
Natan Danous <natan.danous@gmail.com>
2018-07-16 14:04:13 +0200

    Fix affichage responsive

--- resources/assets/react/components/Articles/Article.js
+++ resources/assets/react/components/Articles/Article.js
@@ -37,11 +37,13 @@ class Article extends Component {
 
         return (
             <div className="Article row m-0 my-3 my-md-5">
-                <div className="col-md-3 media mt-1 mb-3 mb-md-0">
-                    <img className="align-self-start img-fluid rounded-circle mr-2" src="http://via.placeholder.com/50x50" />
-                    <div className="media-body">
-                        <Link to={ "/assos/" + this.props.article.collaborators[0].login }>{ this.props.article.collaborators[0].shortname }</Link>
-                        <span className="d-block text-muted small">{ getTime(this.props.article.created_at) }</span>
+                <div className="col-md-3 mb-3 mb-md-0">
+                    <div className="ml-0 ml-md-4 ml-xl-5 media mt-1">
+                        <img className="align-self-start img-fluid rounded-circle mr-2" src="http://via.placeholder.com/50x50" />
+                        <div className="media-body">
+                            <Link to={ "/assos/" + this.props.article.collaborators[0].login }>{ this.props.article.collaborators[0].shortname }</Link>
+                            <span className="d-block text-muted small">{ getTime(this.props.article.created_at) }</span>
+                        </div>
                     </div>
                 </div>
                 <div className="col-md-7 body mx-auto">
