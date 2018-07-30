88be014618cde289510b5dcec95d82a4fd194dd1
NastuzziSamy <samy@nastuzzi.fr>
2018-07-30 13:18:04 +0200

    Correction d'un bug ne donnant pas les bonnes permissions

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -55,9 +55,9 @@ class Permission extends Model // TODO $must ? $fillable
 
 	public static function getPermission($permissions, string $only_for = null) {
         if (is_string($permissions))
-            return static::findByType($permissions, $is_system);
+            return static::findByType($permissions, $only_for);
         else if (is_int($permissions))
-			return static::find($permissions, $is_system);
+			return static::find($permissions, $only_for);
 		else
 			return $permission;
 	}
