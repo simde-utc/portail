f2a7304daf9e1e600710fdb2688837355b97c011
NastuzziSamy <samy@nastuzzi.fr>
2018-08-06 14:00:59 +0200

    Correction d'un bug où le cache des routes rend les scopes inexistants

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -30,6 +31,16 @@ class AuthServiceProvider extends ServiceProvider
 				return new $config['class']();
 			});
 		}
+
+		$this->passport();
+	}
+
+	public function passport() {
+		Passport::tokensCan(\Scopes::all());
+
+	    Passport::tokensExpireIn(now()->addDays(15));
+
+	    Passport::refreshTokensExpireIn(now()->addDays(30));
 	}
 
 	/**
