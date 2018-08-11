c9d2b4c4cad48310e2bd91e156e0d3a649348094
NastuzziSamy <samy@nastuzzi.fr>
2018-08-11 22:13:58 +0200

    Correction de la présence de l'id dans une relation...

--- database/migrations/2018_06_07_132423_create_calendars_events_table.php
+++ database/migrations/2018_06_07_132423_create_calendars_events_table.php
@@ -14,7 +14,6 @@ class CreateCalendarsEventsTable extends Migration
     public function up()
     {
         Schema::create('calendars_events', function (Blueprint $table) {
-            $table->increments('id');
 			$table->integer('calendar_id')->unsigned();
 			$table->foreign('calendar_id')->references('id')->on('calendars');
 			$table->integer('event_id')->unsigned();
