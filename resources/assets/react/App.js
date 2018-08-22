9d47c43ee226ce03c8215199bfbbf7522e87b805
Alex <abrasseur.pro@gmail.com>
2018-08-22 20:47:46 +0200

    Add real RouteNotFound component

--- resources/assets/react/App.js
+++ resources/assets/react/App.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
-
-// Screens
-import ScreensHome from './screens/Home.js';
-import ScreensDashboard from './screens/Dashboard';
-import ScreensAssosList from './screens/AssosList';
-import ScreensAsso from './screens/Asso/Asso.js';
-import ScreensProfile from './screens/Profile';
 
 // Components
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import RouteNotFound from './components/RouteNotFound';
 
-class App extends Component {
+// Screens
+import HomeScreen from './screens/Home.js';
+import DashboardScreen from './screens/Dashboard';
+import AssosListScreen from './screens/AssosList';
+import AssoDetailScreen from './screens/Asso/Asso.js';
+import ProfileScreen from './screens/Profile';
+
+class App extends React.Component {
 	render() {
-		// Fake Components
-		const RouteNotFound = () => (<div><h1>404</h1></div>)
-
 		return (
 			<div className="h-100">
 				<Navbar />
 				<div className="d-flex w-100 h-100">
 					<Sidebar />
 					<Switch>
-						<Route path="/" exact component={ ScreensHome } />
-						<Route path="/dashboard" component={ ScreensDashboard } />
-						<Route path="/assos" exact component={ ScreensAssosList } />
-						<Route path="/assos/:login" component={ ScreensAsso } />
-						<Route path="/profile" component={ ScreensProfile } />
+						<Route path="/" exact component={ HomeScreen } />
+						<Route path="/dashboard" component={ DashboardScreen } />
+						<Route path="/assos" exact component={ AssosListScreen } />
+						<Route path="/assos/:login" component={ AssoDetailScreen } />
+						<Route path="/profile" component={ ProfileScreen } />
 						<Route component={ RouteNotFound } />
 					</Switch>
 				</div>
