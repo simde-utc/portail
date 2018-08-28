d1cdfa90b7d51192bca362425e8887c558e9ab30
Natan Danous <natan.danous@gmail.com>
2018-08-28 12:25:03 +0200

    Cleanup working toolbar

--- resources/assets/react/components/Article/Form.js
+++ resources/assets/react/components/Article/Form.js
@@ -1,90 +1,89 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import SimpleMDE from 'react-simplemde-editor';
+import SimpleMDE from 'simplemde';
+import Editor from 'react-simplemde-editor';
 import "simplemde/dist/simplemde.min.css";
 
 import { getTime } from '../../utils.js';
 
 class ArticleForm extends React.Component {
 	render() {
-		const smdeInstance = require("simplemde");
-
 		return (
-			<SimpleMDE
-				onChange={this.handleChange}
+			<Editor
+				onChange={ this.handleChange }
 				options={{
 					spellChecker: false,
 					toolbar: [
 		                {
 		                    name: "bold",
-		                    action: smdeInstance.toggleBold,
+		                    action: SimpleMDE.toggleBold,
 		                    className: "fas fa-fw fa-bold",
 		                    title: "Gras",
 		                },
 		                {
 		                    name: "italic",
-		                    action: smdeInstance.toggleItalic,
+		                    action: SimpleMDE.toggleItalic,
 		                    className: "fas fa-fw fa-italic",
 		                    title: "Italique",
 		                },
 		                {
 		                    name: "heading",
-		                    action: smdeInstance.toggleHeadingSmaller,
+		                    action: SimpleMDE.toggleHeadingSmaller,
 		                    className: "fas fa-fw fa-heading",
 		                    title: "Titre",
 		                },
 		                {
 		                    name: "quote",
-		                    action: smdeInstance.toggleBlockquote,
+		                    action: SimpleMDE.toggleBlockquote,
 		                    className: "fas fa-fw fa-quote-left",
 		                    title: "Citation",
 		                },
 		                {
 		                    name: "unordered-list",
-		                    action: smdeInstance.toggleUnorderedList,
+		                    action: SimpleMDE.toggleUnorderedList,
 		                    className: "fas fa-fw fa-list-ul",
 		                    title: "Liste non-ordonnée",
 		                },
 		                {
 		                    name: "ordered-list",
-		                    action: smdeInstance.toggleOrderedList,
+		                    action: SimpleMDE.toggleOrderedList,
 		                    className: "fas fa-fw fa-list-ol",
 		                    title: "Liste ordonnée",
 		                },
 		                {
 		                    name: "link",
-		                    action: smdeInstance.drawLink,
+		                    action: SimpleMDE.drawLink,
 		                    className: "fas fa-fw fa-link",
 		                    title: "Insérer un lien",
 		                },
 		                {
 		                    name: "image",
-		                    action: smdeInstance.drawImage,
+		                    action: SimpleMDE.drawImage,
 		                    className: "far fa-fw fa-image",
 		                    title: "Insérer une image",
 		                },
 		                {
 		                    name: "table",
-		                    action: smdeInstance.drawTable,
+		                    action: SimpleMDE.drawTable,
 		                    className: "fas fa-fw fa-table",
 		                    title: "Insérer un tableau",
 		                },
 		                {
 		                    name: "preview",
-		                    action: smdeInstance.togglePreview,
+		                    action: SimpleMDE.togglePreview,
 		                    className: "fas fa-fw fa-eye no-disable",
 		                    title: "Aperçu",
 		                },
 		                {
 		                    name: "side-by-side",
-		                    action: smdeInstance.toggleSideBySide,
+		                    action: SimpleMDE.toggleSideBySide,
 		                    className: "fas fa-fw fa-columns no-disable no-mobile",
 		                    title: "Cote à cote",
 		                },
 		                {
 		                    name: "fullscreen",
-		                    action: smdeInstance.toggleFullScreen,
+		                    action: SimpleMDE.toggleFullScreen,
 		                    className: "fas fa-fw fa-arrows-alt no-disable no-mobile",
 		                    title: "Plein écran",
 		                }
