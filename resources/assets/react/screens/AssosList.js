527d3fbac8558a89f7d46564234115a80a7b70a1
NastuzziSamy <samy@nastuzzi.fr>
2018-09-26 01:35:49 +0200

    Amélioration du listing des assos

--- resources/assets/react/screens/AssosList.js
+++ resources/assets/react/screens/AssosList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import actions from '../redux/actions';
+import { Card, CardBody, CardTitle, CardSubtitle, CardFooter, Button } from 'reactstrap';
+import AspectRatio from 'react-aspect-ratio';
 
 import Block from '../components/Block';
 
@@ -16,23 +18,30 @@ class ScreensAssosList extends React.Component {
 
 	getStage(assos) {
 		return (
-			<div className="d-md-inline-flex flex-wrap">
-				{ assos.map((asso, key) => {
-					var bg = 'bg-' + asso.login;
+			<div className="d-md-inline-flex">
+				<div className="bg-bde align-self-stretch" style={{ width: 100 }}>
+					<span>BDE</span>
+				</div>
+				<div className="d-md-inline-flex flex-wrap">
+					{ assos.map(asso => {
+						var bg = 'bg-' + asso.login;
 
-					if (asso.parent)
-					bg += ' bg-' + asso.parent.login;
+						if (asso.parent)
+						bg += ' bg-' + asso.parent.login;
 
-					return (
-						<Block key={ key }
-							image="http://assos.utc.fr/larsen/style/img/logo-bde.jpg"
-							text={ asso.shortname }
-							class={ bg }
-							style={{ width: 150 }}
-							onClick={() => this.props.history.push('assos/' + asso.login)}
-						/>
-					)
-				})}
+						return (
+							<Card key={ asso.id } className={ "m-2 p-0 " + bg } style={{ width: 200 }} onClick={() => this.props.history.push('assos/' + asso.login)}>
+								<AspectRatio ratio="1" style={{ maxHeight: 150 }} className="d-flex justify-content-center mt-2">
+									<img src={ 'http://assos.utc.fr/larsen/style/img/logo-bde.jpg' } alt="Photo non disponible" className="img-thumbnail" style={{ height: '100%' }} />
+								</AspectRatio>
+								<CardBody>
+									<CardTitle>{ asso.shortname }</CardTitle>
+									<CardSubtitle>{ asso.name }</CardSubtitle>
+								</CardBody>
+							</Card>
+						);
+					})}
+				</div>
 			</div>
 		)
 	}
@@ -41,13 +50,15 @@ class ScreensAssosList extends React.Component {
 		return (
 			<div className="container">
 				<h1 className="title">Liste des associations</h1>
-				<span className={"loader large" + (this.props.fetching ? ' active' : '') }></span>
-				{ this.getStage(this.props.assos.filter(asso => {
-					return asso.login === 'bde' || (asso.parent && asso.parent.login === 'bde')
-				})) }
-				{ this.getStage(this.props.assos.filter(asso => {
-					return asso.login !== 'bde' && (!asso.parent || asso.parent.login !== 'bde')
-				}).sort((asso1, asso2) => asso1.shortname > asso2.shortname)) }
+				<div className="content">
+					<span className={"loader large" + (this.props.fetching ? ' active' : '') }></span>
+					{ this.getStage(this.props.assos.filter(asso => {
+						return asso.login === 'bde' || (asso.parent && asso.parent.login === 'bde')
+					})) }
+					{ this.getStage(this.props.assos.filter(asso => {
+						return asso.login !== 'bde' && (!asso.parent || asso.parent.login !== 'bde')
+					}).sort((asso1, asso2) => asso1.shortname > asso2.shortname)) }
+				</div>
 			</div>
 		);
 	}
