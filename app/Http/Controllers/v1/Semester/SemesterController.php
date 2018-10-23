c10fa7db03b8b04350e39e7a16a776289953b013
NastuzziSamy <samy@nastuzzi.fr>
2018-10-24 01:18:24 +0200

    Linting des controlleurs Semester, Room, Role et Permission

--- app/Http/Controllers/v1/Semester/SemesterController.php
+++ app/Http/Controllers/v1/Semester/SemesterController.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Gère les semestres.
+ *
+ * @author Samy Nastuzzi <samy@nastuzzi.fr>
+ *
+ * @copyright Copyright (c) 2018, SiMDE-UTC
+ * @license GNU GPL-3.0
+ */
 
 namespace App\Http\Controllers\v1\Semester;
 
@@ -7,75 +15,78 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\Semester;
 
-
-/**
- * @resource Semester
- *
- * Gestion des semestres
- */
 class SemesterController extends Controller
 {
-	public function __construct() {
-		$this->middleware(
-			\Scopes::allowPublic()->matchAnyUserOrClient()
-		);
-	}
+    /**
+     * Récupération publique ou sous scopes
+     */
+    public function __construct()
+    {
+        $this->middleware(
+        \Scopes::allowPublic()->matchAnyUserOrClient()
+        );
+    }
 
-	/**
-	 * List Semesters
-	 * @return JsonResponse
-	 */
-	public function index(): JsonResponse {
-		$semesters = Semester::getSelection()->map(function ($semester) {
-			return $semester->hideData();
-		});
+    /**
+     * Liste les semestres.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $semesters = Semester::getSelection()->map(function ($semester) {
+            return $semester->hideData();
+        });
 
-		return response()->json($semesters, 200);
-	}
+        return response()->json($semesters, 200);
+    }
 
-	/**
-	 * Create Semester
-	 *
-	 * @param Request $request
-	 * @return JsonResponse
-	 */
-	public function store(): JsonResponse {
-		abort(405, 'Il n\'est pas possible de créer un semester');
-	}
+    /**
+     * Création non possible (génération automatique).
+     *
+     * @return void
+     */
+    public function store(): JsonResponse
+    {
+        abort(405, 'Il n\'est pas possible de créer un semester');
+    }
 
-	/**
-	 * Show Semester
-	 *
-	 * @param  int $id
-	 * @return JsonResponse
-	 */
-	public function show(string $id): JsonResponse {
-		$semester = Semester::getSemester($id);
+    /**
+     * Montre un semestre.
+     *
+     * @param  string $semester_id
+     * @return JsonResponse
+     */
+    public function show(string $semester_id): JsonResponse
+    {
+        $semester = Semester::getSemester($semester_id);
 
-		if ($semester)
-			return response()->json($semester->hideSubData(), 200);
-		else
-			abort(404, 'Semestre non existant');
-	}
+        if ($semester) {
+            return response()->json($semester->hideSubData(), 200);
+        } else {
+            abort(404, 'Semestre non existant');
+        }
+    }
 
-	/**
-	 * Update Semester
-	 *
-	 * @param Request $request
-	 * @param  int $id
-	 * @return JsonResponse
-	 */
-	public function update(string $id): JsonResponse {
-		abort(405, 'Il n\'est pas possible de modifier un semester');
-	}
+    /**
+     * Mise à jour non possible (génération automatique).
+     *
+     * @param  string $semester_id
+     * @return void
+     */
+    public function update(string $semester_id): JsonResponse
+    {
+        abort(405, 'Il n\'est pas possible de modifier un semester');
+    }
 
-	/**
-	 * Delete Semester
-	 *
-	 * @param  int $id
-	 * @return JsonResponse
-	 */
-	public function destroy(): JsonResponse {
-		abort(405, 'Il n\'est pas possible de supprimer un semester');
-	}
+    /**
+     * Suppression non possible (génération automatique).
+     *
+     * @param  string $semester_id
+     * @return void
+     */
+    public function destroy(string $semester_id): JsonResponse
+    {
+        abort(405, 'Il n\'est pas possible de supprimer un semester');
+    }
 }
