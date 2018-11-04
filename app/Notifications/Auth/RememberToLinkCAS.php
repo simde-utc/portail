b2fa7fa3044e1fc5b8f2aae31df4196f8ae959e3
NastuzziSamy <samy@nastuzzi.fr>
2018-11-04 11:53:26 +0100

    Linting des notifications

--- app/Notifications/Auth/RememberToLinkCAS.php
+++ app/Notifications/Auth/RememberToLinkCAS.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Notification pour rappeler de connecter son compte CAS à un mot de passe.
+ *
+ * @author Samy Nastuzzi <samy@nastuzzi.fr>
+ *
+ * @copyright Copyright (c) 2018, SiMDE-UTC
+ * @license GNU GPL-3.0
+ */
 
 namespace App\Notifications\Auth;
 
@@ -9,31 +17,65 @@ use Carbon\Carbon;
 
 class RememberToLinkCAS extends Notification
 {
-    public function __construct() {
+    /**
+     * Déclare le type de notification.
+     */
+    public function __construct()
+    {
         parent::__construct('user');
     }
 
-    protected function getAction(CanBeNotifiable $notifiable) {
+    /**
+     * Action réalisable via la notification.
+     * @param  CanBeNotifiable $notifiable
+     * @return array
+     */
+    protected function getAction(CanBeNotifiable $notifiable)
+    {
         return [
             'name' => 'Lier mon compte CAS/Portail avec une adresse email',
             'url' => url('login/cas/link'),
         ];
     }
 
-    protected function getSubject(CanBeNotifiable $notifiable) {
+    /**
+     * Sujet de la notification.
+     *
+     * @param  CanBeNotifiable $notifiable
+     * @return string
+     */
+    protected function getSubject(CanBeNotifiable $notifiable)
+    {
         return 'Liez votre compte !';
     }
 
-    protected function getContent(CanBeNotifiable $notifiable) {
-        return 'Pensez à lier votre compte CAS/Portail avec une adresse email. Ceci vous permettra de vous connecter à votre compte lors que votre CAS ne sera plus valide';
+    /**
+     * Contenu texte de la notification.
+     *
+     * @param  CanBeNotifiable $notifiable
+     * @return string
+     */
+    protected function getContent(CanBeNotifiable $notifiable)
+    {
+        return 'Pensez à lier votre compte CAS/Portail avec une adresse email.
+            Ceci vous permettra de vous connecter à votre compte lors que votre CAS ne sera plus valide';
     }
 
-    protected function getMailBody(CanBeNotifiable $notifiable, MailMessage $mail) {
+    /**
+     * Contenu email de la notification.
+     *
+     * @param  CanBeNotifiable $notifiable
+     * @param  MailMessage     $mail
+     * @return MailMessage
+     */
+    protected function getMailBody(CanBeNotifiable $notifiable, MailMessage $mail)
+    {
         return $mail
             ->success()
             ->line($notifiable->name)
             ->line('Pensez à lier votre compte CAS/Portail avec une adresse email.')
             ->line('Pour rappel, votre compte a été créé le '.Carbon::parse($notifiable->created_at)->format('d/m/Y à H:i'))
-            ->line('En faisant celà, il vous est possible d\'accéder à votre compte même si vous ne faites plus parti de l\'UTC et donc garder vos préférences et votre parcours associatif par exemple.');
+            ->line('En faisant celà, il vous est possible d\'accéder à votre compte même si vous ne faites plus parti
+                de l\'UTC et donc garder vos préférences et votre parcours associatif par exemple.');
     }
 }
--- app/Notifications/User/UserDesactivation.php
+++ app/Notifications/User/UserDesactivation.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Notification pour indiquer la désaction du compte.
+ *
+ * @author Samy Nastuzzi <samy@nastuzzi.fr>
+ *
+ * @copyright Copyright (c) 2018, SiMDE-UTC
+ * @license GNU GPL-3.0
+ */
 
 namespace App\Notifications\User;
 
@@ -8,19 +16,45 @@ use Illuminate\Notifications\Messages\MailMessage;
 
 class UserDesactivation extends Notification
 {
-    public function __construct() {
+    /**
+     * Déclare le type de notification.
+     */
+    public function __construct()
+    {
         parent::__construct('user');
     }
 
-    protected function getSubject(CanBeNotifiable $notifiable) {
+    /**
+     * Sujet de la notification.
+     *
+     * @param  CanBeNotifiable $notifiable
+     * @return string
+     */
+    protected function getSubject(CanBeNotifiable $notifiable)
+    {
         return 'Désactivation de votre compte utilisateur';
     }
 
-    protected function getContent(CanBeNotifiable $notifiable) {
+    /**
+     * Contenu texte de la notification.
+     *
+     * @param  CanBeNotifiable $notifiable
+     * @return string
+     */
+    protected function getContent(CanBeNotifiable $notifiable)
+    {
         return 'Désactivation du compte';
     }
 
-    protected function getMailBody(CanBeNotifiable $notifiable, MailMessage $mail) {
+    /**
+     * Contenu email de la notification.
+     *
+     * @param  CanBeNotifiable $notifiable
+     * @param  MailMessage     $mail
+     * @return MailMessage
+     */
+    protected function getMailBody(CanBeNotifiable $notifiable, MailMessage $mail)
+    {
         return $mail
             ->error()
             ->line($notifiable->name)
