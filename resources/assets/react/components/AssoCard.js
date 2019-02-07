320887e9f9317e61bc610008526df3d960f3a21a
Matt Glorion <matt@glorion.fr>
2019-02-07 16:50:16 +0100

    refacto

--- resources/assets/react/components/AssoCard.js
+++ resources/assets/react/components/AssoCard.js
@@ -5,26 +5,26 @@
  *
  * @copyright Copyright (c) 2019, SiMDE-UTC
  * @license GNU GPL-3.0
-**/
+ **/
 
 import React from 'react';
 import bdeImage from '../../images/bde.jpg'
 
-class AssoCard extends React.Component {
-	render() {
-		let image = ((this.props.image == null) ? bdeImage : this.props.image);
-
-		return <div className="asso-card">
-			<div className="thumbnail"
-				 style={{backgroundImage: "url('" + image + "')"}}>
-				<div className={ "overlay " + this.props.login}>{ this.props.name }</div>
-			</div>
-			<div className="name-container">
-				<div className="asso-shortname">{ this.props.shortname }</div>
-			</div>
-			<div className={"card-line "+ this.props.login}/>
-		</div>;
-	}
-}
+const AssoCard = ({image, login, name, shortname}) => {
+    const _image = !image ? bdeImage : image;
+    return <div className="asso-card">
+        <div
+            className="thumbnail"
+            style={{backgroundImage: "url('" + _image + "')"}}>
+            <div className={"overlay " + login}>
+                {name}
+            </div>
+        </div>
+        <div className="name-container">
+            <div className="asso-shortname">{shortname}</div>
+        </div>
+        <div className={"card-line " + login}/>
+    </div>;
+};
 
 export default AssoCard;
