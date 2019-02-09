2854bf19ed0f33ce7f0710af166323425bc5a4aa
NastuzziSamy <samy@nastuzzi.fr>
2019-02-09 17:43:47 +0100

    Application du linter sur les fichiers d'entrée du frontend

--- resources/assets/react/App.js
+++ resources/assets/react/App.js
@@ -6,20 +6,20 @@
  *
  * @copyright Copyright (c) 2018, SiMDE-UTC
  * @license GNU GPL-3.0
-**/
+ */
 
 import React from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import 'react-notifications/lib/notifications.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 // Components
+import { NotificationContainer } from 'react-notifications';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import ErrorCatcher from './routes/ErrorCatcher';
 import NotFoundRoute from './routes/NotFound';
 import LoggedRoute from './routes/Logged';
-import { NotificationContainer } from 'react-notifications';
 
 // Screens
 import AppLoader from './AppLoader';
@@ -30,33 +30,28 @@ import ServicesListScreen from './screens/ServicesList';
 import AssoDetailScreen from './screens/Asso';
 import ProfileScreen from './screens/Profile';
 
+const App = () => (
+	<div className="h-100">
+		<AppLoader />
+		<Navbar />
 
-class App extends React.Component {
-	render() {
-		return (
-			<div className="h-100">
-				<AppLoader />
-				<Navbar />
+		<div className="d-flex w-100 h-100">
+			<Sidebar />
+			<ErrorCatcher>
+				<Switch>
+					<Route path="/" exact component={HomeScreen} />
+					<Route path="/dashboard" component={DashboardScreen} />
+					<Route path="/assos" exact component={AssosListScreen} />
+					<Route path="/assos/:login" component={AssoDetailScreen} />
+					<Route path="/services" exact component={ServicesListScreen} />
+					<LoggedRoute path="/profile" component={ProfileScreen} />
+					<Route component={NotFoundRoute} />
+				</Switch>
+			</ErrorCatcher>
+		</div>
 
-				<div className="d-flex w-100 h-100">
-					<Sidebar />
-					<ErrorCatcher>
-						<Switch>
-							<Route path="/" exact component={ HomeScreen } />
-							<Route path="/dashboard" component={ DashboardScreen } />
-							<Route path="/assos" exact component={ AssosListScreen } />
-							<Route path="/assos/:login" component={ AssoDetailScreen } />
-							<Route path="/services" exact component={ ServicesListScreen } />
-							<LoggedRoute path="/profile" component={ ProfileScreen } />
-							<Route component={ NotFoundRoute } />
-						</Switch>
-					</ErrorCatcher>
-				</div>
-
-				<NotificationContainer />
-			</div>
-		);
-	}
-}
+		<NotificationContainer />
+	</div>
+);
 
 export default App;
--- resources/assets/react/bootstrap.js
+++ resources/assets/react/bootstrap.js
@@ -1,36 +1,35 @@
-import store from './redux/store.js';
+/**
+ * Préparation de l'application
+ *
+ * @author Samy Nastuzzi <samy@nastuzzi.fr>
+ * @author Natan Danous <natous.danous@hotmail.fr>
+ *
+ * @copyright Copyright (c) 2018, SiMDE-UTC
+ * @license GNU GPL-3.0
+ */
+
+import { store } from './redux/store';
+
 window._ = require('lodash');
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
+// Permet d'exécuter des requêtes Ajax simplement vers le Portail
 window.axios = require('axios');
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Next we will register the CSRF Token as a common header with Axios so that
- * all outgoing HTTP requests automatically have it attached. This is just
- * a simple convenience so we don't have to attach every token manually.
- */
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-if (token)
+// Récupération du token CSRF
+const token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
 	window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-else
+}
+else {
 	console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
-/**
- * Attach Axios Interceptors
- */
-
+// Ajout un intercepter de réponse.
 window.axios.interceptors.response.use(
 	response => response,
 	error => {
-		// Deal with Unauthenticated requests
+		// Récupération des requêtes HTTP 401
 		if (error.response.status === 401 && store.resources) {
 			store.resources.user = {};
 		}
@@ -38,7 +37,6 @@ window.axios.interceptors.response.use(
 		return Promise.reject(error);
 	}
 );
-
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
