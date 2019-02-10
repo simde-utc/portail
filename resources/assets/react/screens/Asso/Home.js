cb1479cf76d657205555ac7a797631e7a5fd6bcd
NastuzziSamy <samy@nastuzzi.fr>
2019-02-10 12:23:11 +0100

    Application du linter sur les pages associations

--- resources/assets/react/screens/Asso/Home.js
+++ resources/assets/react/screens/Asso/Home.js
@@ -16,110 +16,141 @@ import Img from '../../components/Image';
 	roles: store.getData(['assos', props.asso.id, 'roles']),
 }))
 class AssoHomeScreen extends React.Component {
-  componentWillMount() {
-    if (this.props.asso.id) {
-      this.loadAssosData(this.props.asso.id);
-    }
-  }
+	componentWillMount() {
+		const {
+			asso: { id },
+		} = this.props;
 
-  componentWillReceiveProps(props) {
-    if (this.props.asso.id !== props.asso.id) {
-      this.loadAssosData(props.asso.id);
-    }
-  }
+		if (id) {
+			this.loadAssosData(id);
+		}
+	}
 
-  loadAssosData(id) {
-		this.props.dispatch(actions.assos(id).contacts.all());
+	componentWillReceiveProps({ asso: { id } }) {
+		const { asso } = this.props;
+
+		if (asso.id !== id) {
+			this.loadAssosData(id);
+		}
 	}
 
 	getFollowButton(isFollowing, isMember) {
+		const { follow, unfollow } = this.props;
+
 		if (isFollowing && !isMember) {
 			return (
-				<Button className="m-1 btn btn-sm" color="danger" outline onClick={ this.props.unfollow }>
+				<Button className="m-1 btn btn-sm" color="danger" outline onClick={unfollow}>
 					Ne plus suivre
 				</Button>
-			)
+			);
 		}
-		else {
-			if (isMember) {
-				return (
-					<Button className="m-1 btn btn-sm" outline disabled>
-						Suivre
-					</Button>
-				)
-			}
-			else  {
-				return (
-					<Button className="m-1 btn btn-sm" color="primary" outline onClick={ this.props.follow }>
-						Suivre
-					</Button>
-				)
-			}
+
+		if (isMember) {
+			return (
+				<Button className="m-1 btn btn-sm" outline disabled>
+					Suivre
+				</Button>
+			);
 		}
+
+		return (
+			<Button className="m-1 btn btn-sm" color="primary" outline onClick={follow}>
+				Suivre
+			</Button>
+		);
 	}
 
 	getMemberButton(isMember, isFollowing, isWaiting) {
+		const { leave, join } = this.props;
+
 		if (isMember) {
 			if (isWaiting) {
 				return (
-					<Button className="m-1 btn btn-sm" color="warning" outline onClick={() => { this.props.leave && this.props.leave(true) }}>
+					<Button
+						className="m-1 btn btn-sm"
+						color="warning"
+						outline
+						onClick={() => {
+							leave(true);
+						}}
+					>
 						En attente...
 					</Button>
-				)
+				);
 			}
-			else  {
-				return (
-					<Button className="m-1 btn btn-sm" color="danger" outline onClick={() => { this.props.leave && this.props.leave(false) }}>
-						Quitter
-					</Button>
-				)
-			}
+
+			return (
+				<Button
+					className="m-1 btn btn-sm"
+					color="danger"
+					outline
+					onClick={() => {
+						leave(false);
+					}}
+				>
+					Quitter
+				</Button>
+			);
 		}
-		else  {
-			if (isFollowing) {
-				return (
-					<Button className="m-1 btn btn-sm" outline disabled>
-						Rejoindre
-					</Button>
-				)
-			}
-			else  {
-				return (
-					<Button className="m-1 btn btn-sm btn" color="primary" outline onClick={ this.props.join }>
-						Rejoindre
-					</Button>
-				)
-			}
+
+		if (isFollowing) {
+			return (
+				<Button className="m-1 btn btn-sm" outline disabled>
+					Rejoindre
+				</Button>
+			);
 		}
+
+		return (
+			<Button className="m-1 btn btn-sm btn" color="primary" outline onClick={join}>
+				Rejoindre
+			</Button>
+		);
+	}
+
+	loadAssosData(id) {
+		const { dispatch } = this.props;
+
+		dispatch(actions.assos(id).contacts.all());
 	}
 
 	render() {
-		const asso = this.props.asso;
+		const {
+			asso,
+			isAuthenticated,
+			userIsFollowing,
+			userIsMember,
+			userIsWaiting,
+			contacts,
+			contactsFailed,
+		} = this.props;
 
-		let color = 'color-' + asso.login;
+		let color = `color-${asso.login}`;
 
-		if (asso.parent)
-			color += ' color-' + asso.parent.login;
+		if (asso.parent) color += ` color-${asso.parent.login}`;
 
 		return (
 			<div className="container">
-				{ (asso) ? (
+				{asso ? (
 					<div className="row">
 						<div className="col-md-2 mt-3 px-1 d-flex flex-md-column">
 							<AspectRatio className="mb-2" ratio="1">
-								<Img image={ asso.image } style={{ width: "100%" }} />
+								<Img image={asso.image} style={{ width: '100%' }} />
 							</AspectRatio>
-							{ this.props.isAuthenticated && this.getFollowButton(this.props.userIsFollowing, this.props.userIsMember) }
-							{ this.props.isAuthenticated && this.getMemberButton(this.props.userIsMember, this.props.userIsFollowing, this.props.userIsWaiting) }
+							{isAuthenticated && this.getFollowButton(userIsFollowing, userIsMember)}
+							{isAuthenticated &&
+								this.getMemberButton(userIsMember, userIsFollowing, userIsWaiting)}
 						</div>
 						<div className="col-md-8">
-							<h1 className={ "title " + color }>{ asso.shortname } <small className="text-muted h4">{ asso.name }</small></h1>
-							<span className="mt-4">{ asso.type && asso.type.description }</span>
-							<ReactMarkdown className="my-3 text-justify" source={ asso.description } />
-							<ContactList className="mt-4" contacts={ this.props.contacts } authorized={ !this.props.contactsFailed } />
+							<h1 className={`title ${color}`}>
+								{asso.shortname} <small className="text-muted h4">{asso.name}</small>
+							</h1>
+							<span className="mt-4">{asso.type && asso.type.description}</span>
+							<ReactMarkdown className="my-3 text-justify" source={asso.description} />
+							<ContactList className="mt-4" contacts={contacts} authorized={!contactsFailed} />
 						</div>
 					</div>
-				) : null }
+				) : null}
 			</div>
 		);
 	}
