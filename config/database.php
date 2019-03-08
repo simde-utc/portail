65b087be859456abbd2311cc299ff40313095188
César Richard <cesar.richard@weezevent.com>
2019-03-08 08:03:07 +0100

    Fuck off nastuzizi

--- config/database.php
+++ config/database.php
@@ -28,7 +28,20 @@ return [
             'strict' => false,
             'engine' => null,
         ],
-
+        'old-portail' => [
+          'driver' => env('OLD_DB_CONNECTION', 'mysql'),
+          'host' => env('OLD_DB_HOST', '127.0.0.1'),
+          'port' => env('OLD_DB_PORT', '3306'),
+          'database' => env('OLD_DB_DATABASE', 'forge'),
+          'username' => env('OLD_DB_USERNAME', 'forge'),
+          'password' => env('OLD_DB_PASSWORD', ''),
+          'unix_socket' => env('OLD_DB_SOCKET', ''),
+          'charset' => 'utf8mb4',
+          'collation' => 'utf8mb4_unicode_ci',
+          'prefix' => '',
+          'strict' => false,
+          'engine' => null,
+        ],
     ],
 
     'migrations' => 'migrations',
