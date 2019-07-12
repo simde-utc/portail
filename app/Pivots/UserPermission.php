fb79fe97b56b86f8b0145dfe5f5b138f77d82c3d
MercierCorentin <corentin.mercier.pro@gmail.com>
2019-07-12 15:45:48 +0200

    app/Pivots folder translated

--- app/Pivots/UserPermission.php
+++ app/Pivots/UserPermission.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Pivot correspondant à la relation permissions - utlisateurs.
+ * Pivot corresponding to the permissions - users relation.
  *
  * @author Samy Nastuzzi <samy@nastuzzi.fr>
  *
@@ -34,7 +34,7 @@ class UserPermission extends Pivot
     ];
 
     /**
-     * Relation avec l'accès.
+     * Relation with access.
      *
      * @return mixed
      */
@@ -44,7 +44,7 @@ class UserPermission extends Pivot
     }
 
     /**
-     * Relation avec le semestre.
+     * Relation with the semester.
      *
      * @return mixed
      */
@@ -54,7 +54,7 @@ class UserPermission extends Pivot
     }
 
     /**
-     * Relation avec l'accès.
+     * Relation with access.
      *
      * @return mixed
      */
@@ -64,7 +64,7 @@ class UserPermission extends Pivot
     }
 
     /**
-     * Relation avec l'utilisateur ayant validé.
+     * Relation with the user user who validated him.
      *
      * @return mixed
      */
