c66b7a694fc3881252b9fff4ee4bba2b77475ce5
MercierCorentin <corentin.mercier.pro@gmail.com>
2019-08-02 19:19:02 +0200

    AssosCareer optimizations and fix mistakes

--- resources/assets/react/screens/Profile/AssociativeCareer.js
+++ resources/assets/react/screens/Profile/AssociativeCareer.js
@@ -32,54 +32,40 @@ class AssociativeCareerScreen extends React.Component {
 	}
 
 	componentDidMount() {
-		const {
-			user: { name },
-			dispatch,
-			rolesFetched,
-			semesters,
-		} = this.props;
+		const { dispatch, rolesFetched, semesters } = this.props;
 		const { associativeSemesters } = this.state;
 
 		if (!rolesFetched) {
 			dispatch(actions.roles.all());
 		}
 
-		dispatch(actions.config({ title: `${name} - Mon Parcours` }));
+		dispatch(actions.config({ title: `Mon Parcours` }));
 
-		semesters
-			.slice()
-			.reverse()
-			.forEach(semester => {
-				if (!associativeSemesters[semester.id]) {
-					actions.user.assos.all({ semester: semester.id }).payload.then(({ data }) => {
-						if (data.length > 0) {
-							this.addNewAssociativeSemester(semester.id, data);
-						}
-					});
-				}
-			});
+		semesters.forEach(semester => {
+			if (!associativeSemesters[semester.id]) {
+				actions.user.assos.all({ semester: semester.id }).payload.then(({ data }) => {
+					if (data.length > 0) {
+						this.addNewAssociativeSemester(semester.id, data);
+					}
+				});
+			}
+		});
 	}
 
 	componentDidUpdate() {
-		const {
-			user: { name },
-			dispatch,
-			rolesFetched,
-		} = this.props;
+		const { dispatch, rolesFetched } = this.props;
 
 		if (!rolesFetched) {
 			dispatch(actions.roles.all());
 		}
 
-		dispatch(actions.config({ title: `${name} - Mon Parcours` }));
+		dispatch(actions.config({ title: `Mon Parcours` }));
 	}
 
 	addNewAssociativeSemester(semester_id, assos) {
-		const { associativeSemesters } = this.state;
-		const objectToAssign = {};
-		objectToAssign[semester_id] = assos;
-		this.setState({
-			associativeSemesters: Object.assign({}, associativeSemesters, objectToAssign),
+		this.setState(prevState => {
+			prevState.associativeSemesters[semester_id] = assos;
+			return prevState;
 		});
 	}
 
@@ -91,31 +77,33 @@ class AssociativeCareerScreen extends React.Component {
 			<div className="ml-5">
 				{rolesFetched &&
 					semestersFetched &&
-					Object.keys(associativeSemesters).map(semester_id => {
-						const semester = semesters.find(semester => semester.id === semester_id);
+					Object.keys(associativeSemesters)
+						.reverse()
+						.map(semester_id => {
+							const semester = semesters.find(semester => semester.id === semester_id);
 
-						const assosBySemesterList = associativeSemesters[semester_id].slice().map(asso => {
-							const roleName = roles.find(role => role.id === asso.pivot.role_id).name;
+							const assosBySemesterList = associativeSemesters[semester_id].map(asso => {
+								const roleName = roles.find(role => role.id === asso.pivot.role_id).name;
 
-							return (
-								<NavLink key={asso.id} to={`/assos/${asso.login}`}>
-									<AssoCard
-										key={asso.id}
-										name={asso.name}
-										shortname={`${asso.shortname} - ${roleName}`}
-										image={asso.image}
-										login={asso.parent ? asso.parent.login : asso.login}
-									/>
-								</NavLink>
-							);
-						});
+								return (
+									<NavLink key={asso.id} to={`/assos/${asso.login}`}>
+										<AssoCard
+											key={asso.id}
+											name={asso.name}
+											shortname={`${asso.shortname} - ${roleName}`}
+											image={asso.image}
+											login={asso.parent ? asso.parent.login : asso.login}
+										/>
+									</NavLink>
+								);
+							});
 
-						const title = assosBySemesterList ? (
-							<h2 style={{ margin: 20 }}>Semestre {semester.name}</h2>
-						) : null;
+							const title = assosBySemesterList ? (
+								<h2 style={{ margin: 20 }}>Semestre {semester.name}</h2>
+							) : null;
 
-						return assosBySemesterList ? [title, ...assosBySemesterList] : null;
-					})}
+							return assosBySemesterList ? [title, ...assosBySemesterList] : null;
+						})}
 			</div>
 		);
 	}
