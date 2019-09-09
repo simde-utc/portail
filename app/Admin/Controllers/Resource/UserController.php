eb40a6066056f6abdd8f315d0f4608d9d2accb09
NastuzziSamy <samy@nastuzzi.fr>
2019-09-09 12:24:03 +0200

    Fix admin contribution permission issue...

--- app/Admin/Controllers/Resource/UserController.php
+++ app/Admin/Controllers/Resource/UserController.php
@@ -51,8 +51,8 @@ class UserController extends Controller
      */
     public function __construct()
     {
-		$this->middleware('permission:user', ['except' => 'contributeBde']);
-        $this->middleware('permission:user-contributorBde', ['only' => 'contributeBde']);
+		$this->middleware('permission:user', ['except' => ['contributeBde']]);
+        $this->middleware('permission:user-contributeBde', ['only' => ['contributeBde']]);
     }
 
     /**
