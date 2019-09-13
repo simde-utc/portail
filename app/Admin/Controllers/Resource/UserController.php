97432abcc5177d91a4ef6da3b387ff9b482a48dd
MercierCorentin <corentin.mercier.pro@gmail.com>
2019-09-13 12:08:40 +0200

    Fix linting mistakes

--- app/Admin/Controllers/Resource/UserController.php
+++ app/Admin/Controllers/Resource/UserController.php
@@ -51,7 +51,7 @@ class UserController extends Controller
      */
     public function __construct()
     {
-		$this->middleware('permission:user', ['except' => ['contributeBde']]);
+        $this->middleware('permission:user', ['except' => ['contributeBde']]);
         $this->middleware('permission:user-contributeBde', ['only' => ['contributeBde']]);
     }
 
