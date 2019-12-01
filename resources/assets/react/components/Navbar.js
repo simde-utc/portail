abda78a121935f67c5e928ee2ff5ce173661e944
Paco <>
2019-12-01 07:05:50 +0100

    [Front] fix nested link warning in profile button

--- resources/assets/react/components/Navbar.js
+++ resources/assets/react/components/Navbar.js
@@ -86,12 +86,10 @@ class Navbar extends React.Component {
 									className="rounded-circle"
 									style={{ backgroundColor: 'rgba(255,255,255,0.5)' }}
 									unloader={
-										<a className="nav-link d-flex" href="/login">
-											<span className="fa-layers fa-lg" style={{ fontSize: 28 }}>
-												<FontAwesomeIcon icon="circle" className="icon-background2" />
-												<FontAwesomeIcon icon="user-alt" transform="shrink-8" />
-											</span>
-										</a>
+										<span className="fa-layers fa-lg" style={{ fontSize: 28 }}>
+											<FontAwesomeIcon icon="circle" className="icon-background2" />
+											<FontAwesomeIcon icon="user-alt" transform="shrink-8" />
+										</span>
 									}
 								/>
 							</NavLink>
