51577b0a8d9e33674d29b13e7ee514172e6584db
Noé AMIOT <amiot.noe@icloud.com>
2020-03-02 23:13:37 +0100

    Added icon for unvalidated associations sidebar + fixed asso visibility in career

--- resources/assets/react/components/Sidebar.js
+++ resources/assets/react/components/Sidebar.js
@@ -4,6 +4,7 @@
  * @author Alexandre Brasseur <abrasseur.pro@gmail.com>
  * @author Samy Nastuzzi <samy@nastuzzi.fr>
  * @author Natan Danous <natous.danous@hotmail.fr>
+ * @author Noé Amiot <noe.amiot@etu.utc.fr>
  *
  * @copyright Copyright (c) 2018, SiMDE-UTC
  * @license GNU GPL-3.0
@@ -29,12 +30,13 @@ class Sidebar extends React.Component {
 	static getAssos(assos) {
 		return assos.map(asso => {
 			let color = `color-${asso.login}`;
+			const icon = asso.pivot.role_id ? 'hourglass' : 'thumbs-up';
 
 			if (asso.parent) color += ` color-${asso.parent.login}`;
 
 			return (
 				<NavLink key={asso.id} className="sidebar-link" to={`/assos/${asso.login}`}>
-					<FontAwesomeIcon icon={asso.pivot.role_id ? 'hands-helping' : 'thumbs-up'} />{' '}
+					<FontAwesomeIcon icon={asso.pivot.validated_by_id ? 'hands-helping' : icon} />{' '}
 					<span className={color}>{asso.shortname}</span>
 				</NavLink>
 			);
