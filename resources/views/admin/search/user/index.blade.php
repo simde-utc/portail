34773d1e7693793eac43532ffc00b15543e2cc94
MercierCorentin <corentin.mercier.pro@gmail.com>
2020-03-13 12:06:50 +0100

    [Admin][UserSearch] UI changes due to UX problems

--- resources/views/admin/search/user/index.blade.php
+++ resources/views/admin/search/user/index.blade.php
@@ -3,6 +3,25 @@
         margin-bottom: 10px;
     }
 
+    hr {
+        margin-top: 30px;
+        margin-bottom: 30px;
+        overflow: visible; /* For IE */
+        padding: 0;
+        border-color: #333;
+        color: #333;
+        text-align: center;
+    }
+
+    hr:after {
+        content: "OU";
+        display: inline-block;
+        position: relative;
+        top: -0.7em;
+        padding: 0 0.25em;
+        background: white;
+    }
+
     .param-add .form-group,.param .form-group {
         margin: 0;
     }
@@ -34,7 +53,19 @@
     .nav>li>a {
         padding: 10px 10px;
     }
+
+    .submit-wrapper{
+        padding-right: 15px;
+        display: flex;
+        justify-content: end;
+    }
 </style>
+
+<script>
+    function handleSubmit(event){
+        document.getElementById("quick_search").value = '';
+    }
+</script>
 
 <div class="row">
     <div class="col-md-6 col-md-offset-3">
@@ -50,31 +81,37 @@
                     </div>
 
                     @if ($errors->has('general'))
-                        <p class="bg-danger">{{ $errors->get('general')[0] }}</p>
+                        <p style="padding:1rem;" class="bg-danger">{{ $errors->get('general')[0] }}</p>
                     @endif
 
-                    @foreach ($fields as $name)
+                    @foreach ($fields as $name => $label)
                         <div class="form-group">
-                            <label for="{{ $name }}" class="col-sm-2 control-label">{{ ucfirst(__($name)) }}</label>
+                            <label for="{{ $name }}" class="col-sm-2 control-label">{{ ucfirst(__($label)) }}</label>
 
                             <div class="col-sm-10">
                                 <input type="text" class="form-control" id="{{ $name }}" name="{{ $name }}">
                             </div>
                         </div>
                     @endforeach
-
-                    <div style="width: 100%; border: SOLID 1px grey"></div>
-                    <br />
+                    <div class="form-group" style="margin-bottom: 0px; margin-left: auto;">
+                        <div class="submit-wrapper">
+                            <button onclick="handleSubmit()" class="btn btn-primary">Rechercher</button>
+                        </div>
+                    </div>
+                </form>
+                <hr>
+                <form class="form-horizontal user-form" method="post">
+                    @csrf
                     <div class="form-group">
-                        <label for="any" class="col-sm-2 control-label">Recherche rapide</label>
+                        <label for="quick_search" class="col-sm-2 control-label">Recherche rapide</label>
 
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="any" name="any">
+                            <input type="text" class="form-control" id="quick_search" name="quick_search">
                         </div>
                     </div>
 
                     <div class="form-group" style="margin-bottom: 0px;">
-                        <div class="col-sm-offset-2 col-sm-10">
+                        <div class="submit-wrapper">
                             <button type="submit" class="btn btn-primary">Rechercher</button>
                         </div>
                     </div>
